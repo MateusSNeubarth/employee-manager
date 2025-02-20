00181f4614145eb54d737b7cbfbb5f1e13b066a4
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-02-20 09:43:58 -0300

    IMPLEMENTED: employee repository, service and controller; exception handler

---
+++ api/src/main/java/com/mateusneubarth/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.mateusneubarth.employeemanager.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mateusneubarth.employeemanager.model.Employee;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
